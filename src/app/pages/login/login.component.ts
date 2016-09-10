0f20dd404dbea6b38c1e3b514a21dff3c2016b65
vitorpfn <vitor.nobrega87@gmail.com>
2016-09-10 15:16:06 +0100

    add login service

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, ViewEncapsulation} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
+import { Http, Headers, HTTP_PROVIDERS } from '@angular/http';
+ import {Router} from '@angular/router';
 
 @Component({
   selector: 'login',
@@ -11,23 +13,46 @@ import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/form
 export class Login {
 
   public form:FormGroup;
-  public email:AbstractControl;
+  public username:AbstractControl;
   public password:AbstractControl;
   public submitted:boolean = false;
+  public http;
+  public router; 
 
-  constructor(fb:FormBuilder) {
+  constructor(http: Http, fb:FormBuilder, router: Router) {
     this.form = fb.group({
-      'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'username': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
       'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
     });
 
-    this.email = this.form.controls['email'];
+    this.username = this.form.controls['username'];
     this.password = this.form.controls['password'];
+    this.http = http;
+    this.router = router;
   }
 
   public onSubmit(values:Object):void {
     this.submitted = true;
     if (this.form.valid) {
+      let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+      this.http.post('auth/index/login', this.form.value,
+      {
+      headers: headers
+      })
+        .subscribe(
+          data => {
+           console.log('success response:');
+           console.log(data);
+          },
+          err =>  {
+           console.log('error response:');
+           console.log(err);
+           debugger;
+           this.router.navigate(['/pages/dashboard']);
+          },
+          () => console.log('Authentication Complete')
+        );
       // your code goes here
       // console.log(values);
     }
